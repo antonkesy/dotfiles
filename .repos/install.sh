5c2e40b6b1a90b2cc725888a6e3b622cc26ec7ca
Anton Kesy <anton@kesy.de>
2023-07-06 20:57:12 +0200

    add neovim

--- .repos/install.sh
+++ .repos/install.sh
@@ -5,3 +5,12 @@
 # sensor
 sudo apt install lm-sensors
 
+# neovim
+# https://github.com/neovim/neovim
+sudo apt-get install ninja-build gettext cmake unzip curl
+sudo apt install cmake
+cd neovim
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+cd ..
+
