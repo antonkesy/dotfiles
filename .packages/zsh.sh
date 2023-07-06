6f8af41e43dc34cc5266850af4fd25c346ac9e90
Anton Kesy <anton@kesy.de>
2023-07-06 20:57:41 +0200

    add theme + fonts

--- .packages/zsh.sh
+++ .packages/zsh.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+sudo apt install unzip
+
+# hack font
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip
+sudo unzip ./Hack.zip -d /usr/share/fonts/truetype/
+sudo fc-cache -f -v
+rm ./Hack.zip
+
+# starship theme
+# https://starship.rs/
+curl -sS https://starship.rs/install.sh | sh
+
 sudo apt install zsh -y
 # set zsh as default shell
 chsh -s $(which zsh)
+
 
 # https://github.com/ohmyzsh/ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
