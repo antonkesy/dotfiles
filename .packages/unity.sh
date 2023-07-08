d494976ef2df7b13c1f6d1b4ae04c78a09cd9496
Anton Kesy <anton@kesy.de>
2023-07-08 23:30:21 +0200

    add steam

--- .packages/unity.sh
+++ .packages/unity.sh
@@ -11,4 +11,4 @@ sudo apt-get install libssl1.1
 sudo sh -c 'echo "deb https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
 wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
 sudo apt update
-sudo apt install unityhub blender -y
+sudo apt install unityhub blender steam -y
