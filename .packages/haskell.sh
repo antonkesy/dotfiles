3afe8ad23be174e43142925869d835d662566b54
Anton Kesy <antonkesy@gmail.com>
2023-07-08 23:30:51 +0200

    install stack with sudo

--- .packages/haskell.sh
+++ .packages/haskell.sh
@@ -4,5 +4,5 @@
 sudo apt-get install ghc haskell-platform cabal-install -y
 
 # https://docs.haskellstack.org/en/stable/install_and_upgrade/
-curl -sSL https://get.haskellstack.org/ | sh
+curl -sSL https://get.haskellstack.org/ | sudo sh
 
