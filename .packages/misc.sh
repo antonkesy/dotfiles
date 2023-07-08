b8af6f820afdeb2eb2bf24d312c22c0e063a425b
Anton Kesy <anton@kesy.de>
2023-07-08 23:30:43 +0200

    add curl

--- .packages/misc.sh
+++ .packages/misc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # all small packages without grouping
 
-sudo apt install -y ffmpeg flex pandoc pdfarranger thunderbird libreoffice gimp baobab
+sudo apt install -y ffmpeg flex pandoc pdfarranger thunderbird libreoffice gimp baobab curl
 sudo snap install krita postman 1password obs-studio
