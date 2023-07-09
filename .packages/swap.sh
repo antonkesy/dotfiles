7d09271fe93877632b37f48e9475419f24517082
Anton Kesy <anton@kesy.de>
2023-07-09 09:53:15 +0200

    add 100G swap file creation

---
+++ .packages/swap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo fallocate -l 100G /swapfile &&
+ls -lh /swapfile &&
+sudo chmod 600 /swapfile &&
+sudo mkswap /swapfile &&
+sudo swapon /swapfile &&
+sudo cp /etc/fstab /etc/fstab.bak &&
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
