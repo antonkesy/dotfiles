3998841e14ac88d35d690bf5eab148fdcbdec4d9
Anton Kesy <antonkesy@gmail.com>
2023-07-11 18:29:43 +0200

    update misc

--- .packages/misc.sh
+++ .packages/misc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # all small packages without grouping
 
-sudo apt install -y ffmpeg flex pandoc pdfarranger thunderbird libreoffice gimp baobab curl
-sudo snap install krita postman 1password obs-studio
+sudo apt install -y ffmpeg flex pandoc pdfarranger thunderbird libreoffice gimp baobab curl qt6-base-dev anki python3-distutils
+sudo snap install krita postman obs-studio
