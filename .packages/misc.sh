8e8642a075bc2c743108f437b629ce96e3a46e39
Anton Kesy <antonkesy@gmail.com>
2023-07-22 16:11:57 +0200

    add pre-commit

--- .packages/misc.sh
+++ .packages/misc.sh
@@ -3,3 +3,4 @@
 
 sudo apt install -y ffmpeg flex pandoc pdfarranger thunderbird libreoffice gimp baobab curl qt6-base-dev anki python3-distutils
 sudo snap install krita postman obs-studio
+pip install pre-commit
