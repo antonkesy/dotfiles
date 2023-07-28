53c0f28950ef62be738fe95926766f988839c267
Anton Kesy <antonkesy@gmail.com>
2023-07-28 13:41:38 +0200

    update arduino install

--- .packages/arduino.sh
+++ .packages/arduino.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # https://arduino.github.io/arduino-cli/0.33/installation/
-curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+# curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+
+# https://arduino.github.io/arduino-cli/0.33/installation/#install-via-homebrew-macoslinux
+brew update
+brew install arduino-cli
