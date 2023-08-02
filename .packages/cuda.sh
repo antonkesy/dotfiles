235772064bda18cbe9455b53623597325a202734
Anton Kesy <antonkesy@gmail.com>
2023-08-02 14:05:44 +0200

    decrease nvidia driver version

--- .packages/cuda.sh
+++ .packages/cuda.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # working nvidia driver with 22.04 Ubuntu and Razer Core X Chroma
-# sudo apt install nvidia-dkms-535 nvidia-utils-535
+sudo apt install nvidia-driver-530 nvidia-utils-530
 
 sudo apt install nvidia-cuda-toolkit
+sudo prime-select nvidia
 
