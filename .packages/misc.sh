0eb9686a8a4c5102b930b377f4297980e29dbc6e
Anton Kesy <anton@kesy.de>
2023-09-01 18:55:34 +0200

    add more latex

--- .packages/misc.sh
+++ .packages/misc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # all small packages without grouping
 
-sudo apt install -y ffmpeg flex pandoc pdfarranger thunderbird libreoffice gimp baobab curl qt6-base-dev anki python3-distutils texstudio
+sudo apt install -y ffmpeg flex pandoc pdfarranger thunderbird libreoffice gimp baobab curl qt6-base-dev anki python3-distutils latexmk texstudio texlive-full
 sudo snap install krita postman obs-studio
 pip install pre-commit
