e92fed4ff3da69c31b1db3526e627782c1ed0823
Anton Kesy <anton@kesy.de>
2023-09-01 18:36:52 +0200

    add texstudio

--- .packages/misc.sh
+++ .packages/misc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # all small packages without grouping
 
-sudo apt install -y ffmpeg flex pandoc pdfarranger thunderbird libreoffice gimp baobab curl qt6-base-dev anki python3-distutils
+sudo apt install -y ffmpeg flex pandoc pdfarranger thunderbird libreoffice gimp baobab curl qt6-base-dev anki python3-distutils texstudio
 sudo snap install krita postman obs-studio
 pip install pre-commit
