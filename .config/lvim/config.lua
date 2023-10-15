2420210902776e505544003739511f37ef59ffcf
Anton Kesy <antonkesy@gmail.com>
2023-10-15 16:48:57 +0200

    updates

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -238,24 +238,6 @@ lvim.plugins = {
     end,
   },
   {
-    "folke/persistence.nvim",
-    event = "BufReadPre", -- this will only start session saving when an actual file was opened
-    -- module = "persistence", -- deprecated
-    config = function()
-      require("persistence").setup {
-        dir = vim.fn.expand(vim.fn.stdpath "config" .. "/session/"),
-        options = { "buffers", "curdir", "tabpages", "winsize" },
-      }
-
-      lvim.builtin.which_key.mappings["S"] = {
-        name = "Session",
-        c = { "<cmd>lua require('persistence').load()<cr>", "Restore last session for current dir" },
-        l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
-        Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
-      }
-    end,
-  },
-  {
     "rmagatti/auto-session",
     config = function()
       require("auto-session").setup {
