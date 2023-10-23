628b31cd5762f1076378007ca0085bfdf6b8b488
Anton Kesy <anton@kesy.de>
2023-10-23 11:47:37 +0200

    update lvim

--- .packages/lvim.sh
+++ .packages/lvim.sh
@@ -3,6 +3,7 @@
 sudo apt install luarocks xclip
 
 # https://www.lunarvim.org/docs/installation
-LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
-
+bash ~/.local/share/lunarvim/lvim/utils/installer/uninstall.sh
+# LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) --yes
 pip install neovim
