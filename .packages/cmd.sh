7d89956647f0361f9e24af6267840b5078313013
Anton Kesy <antonkesy@gmail.com>
2023-10-24 21:52:29 +0200

    update tmux

--- .packages/cmd.sh
+++ .packages/cmd.sh
@@ -5,3 +5,5 @@ pip install trash-cli
 
 # unbind alt to avoaid conflict with tmux
 gsettings set org.gnome.desktop.wm.keybindings activate-window-menu "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-group "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-group-backward "[]"
