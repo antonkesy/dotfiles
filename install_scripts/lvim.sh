38b766bee6d78fe622683f56b9fd713b9be2cf4b
Anton Kesy <anton@kesy.de>
2023-10-28 13:00:50 +0200

    add vale

--- install_scripts/lvim.sh
+++ install_scripts/lvim.sh
@@ -7,3 +7,9 @@ bash ~/.local/share/lunarvim/lvim/utils/installer/uninstall.sh
 # LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) --yes
 pip install neovim
+
+# vale (https://github.com/errata-ai/vale)
+pip install vale
+cd ~
+vale sync
+cd -
