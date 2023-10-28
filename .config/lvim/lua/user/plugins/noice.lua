deaacc2ef59b31a2f25292a5061c66683aa588e6
Anton Kesy <antonkesy@gmail.com>
2023-10-28 21:40:06 +0200

    remove notify from noice

--- .config/lvim/lua/user/plugins/noice.lua
+++ .config/lvim/lua/user/plugins/noice.lua
@@ -1,24 +1,15 @@
 return {
   {
-    -- TODO config: https://github.com/folke/noice.nvim
-    -- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {
-      -- add any options here
-    },
     dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
+      -- Disabled notify because its to intrusive in my opinion
+      -- "rcarriga/nvim-notify",
     },
     config = function()
       require("noice").setup({
         -- enable macro recording
-        -- TODO: nvim-tree + mason still show pop-ups
         routes = {
           {
             view = "mini",
@@ -41,7 +32,6 @@ return {
 
 
         lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
           override = {
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
             ["vim.lsp.util.stylize_markdown"] = true,
@@ -56,11 +46,11 @@ return {
         },
 
         presets = {
-          bottom_search = false,        -- use a classic bottom cmdline for search
+          bottom_search = false,
           command_palette = false,      -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false,       -- add a border to hover docs and signature help
+          inc_rename = true,            -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = true,        -- add a border to hover docs and signature help
         },
       })
     end
