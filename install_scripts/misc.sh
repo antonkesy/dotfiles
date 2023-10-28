1a4a36dd32e8b419f31035e90f16777d45f304a3
Anton Kesy <anton@kesy.de>
2023-10-28 10:48:54 +0200

    download in mp3 instead of m4a for compatibility

--- install_scripts/misc.sh
+++ install_scripts/misc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # all small packages without grouping
 
-sudo apt install -y ffmpeg flex pandoc pdfarranger thunderbird libreoffice gimp baobab curl qt6-base-dev anki python3-distutils latexmk texstudio texlive-full
+sudo apt install -y ffmpeg flex pandoc pdfarranger thunderbird libreoffice gimp baobab curl qt6-base-dev anki python3-distutils latexmk texstudio texlive-full ubuntu-restricted-extras
 sudo snap install krita postman obs-studio
 pip install pre-commit
--- usefull_scripts/youtube-download-music-playlist.sh
+++ usefull_scripts/youtube-download-music-playlist.sh
@@ -8,9 +8,15 @@ fi
 youtube_link="$1"
 
 # get playlist title and create directory
-playlist_title=$(yt-dlp -f 140 --skip-download --print playlist_title --no-warnings -I 1:1 ${youtube_link})
+playlist_title=$(yt-dlp --skip-download --print playlist_title --no-warnings -I 1:1 ${youtube_link})
+
+if [ -z "$playlist_title" ]; then
+    echo "Could not fetch playlist title :/ Set directory name to timecode"
+    playlist_title=$(date +%s)
+fi
+
 mkdir "$playlist_title"
 cd "$playlist_title" || exit
 
 # download playlist
-yt-dlp -f 140 "${youtube_link}"
+yt-dlp --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 160K --output "%(title)s.%(ext)s" --yes-playlist "${youtube_link}"
