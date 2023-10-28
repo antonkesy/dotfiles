f060c82b46415cadd03e1ef9b56cdfe2467309c7
Anton Kesy <antonkesy@gmail.com>
2023-10-28 17:13:56 +0200

    add broken vale-ls

---
+++ .config/lvim/lua/user/lsp.lua
@@ -0,0 +1,27 @@
+vim.lsp.log_levels = 0
+lvim.builtin.mason.on_config_done = function()
+  local lspconfig = require('lspconfig')
+  lspconfig.vale_ls.setup {
+    single_file_support = true,
+    filetypes = { "markdown", "tex", "text" },
+    -- not working :/
+    -- init_options = {
+    -- configPath = vim.env.XDG_CONFIG_HOME .. "/vale/.vale.ini"
+    -- },
+  }
+end
+
+-- require("null-ls").setup({
+--   sources = {
+--     require("null-ls").builtins.diagnostics.vale,
+--   },
+-- })
+
+require("mason-lspconfig").setup_handlers {
+  ["pyright"] = function()
+    local lspconfig = require('lspconfig')
+    lspconfig.pyright.setup {
+      autoImportCompletion = true,
+    }
+  end
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # dotfiles
 
 All files in this repository are relative to the home directory.
+
+The is is here
