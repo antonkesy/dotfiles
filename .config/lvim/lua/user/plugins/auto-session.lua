51264507cce33392c0e41f34999a944500baea3c
Anton Kesy <anton@kesy.de>
2023-11-03 19:01:33 +0100

    disable minimap

--- .config/lvim/lua/user/plugins/auto-session.lua
+++ .config/lvim/lua/user/plugins/auto-session.lua
@@ -19,15 +19,15 @@ return {
       local function restore_nvim_tree()
         require("nvim-tree.api").tree.open({ focus = false })
         require("nvim-tree.api").tree.close()
-        vim.cmd("Minimap")
+        -- vim.cmd("Minimap")
       end
 
-      local function close_minimap()
-        vim.cmd("MinimapClose")
-      end
+      -- local function close_minimap()
+      --   vim.cmd("MinimapClose")
+      -- end
 
       require('auto-session').setup {
-        pre_save_cmds = { close_minimap },
+        -- pre_save_cmds = { close_minimap },
         post_restore_cmds = { restore_nvim_tree }
       }
     end,
