b0398950b92dfd775eb008a777d6e2fe1b321515
Anton Kesy <antonkesy@gmail.com>
2023-11-04 20:18:16 +0100

    format

--- link.sh
+++ link.sh
@@ -8,44 +8,44 @@ mkdir -p "$HOME/.repos"
 mkdir -p "$HOME/.tmux/plugins"
 
 dirs_to_link=(
-  ".config/lvim/lua/"
-  ".repos/antigen/"
-  ".repos/gnome-shell-extension-clipboard-indicator/"
-  ".tmux/plugins/tpm/"
+    ".config/lvim/lua/"
+    ".repos/antigen/"
+    ".repos/gnome-shell-extension-clipboard-indicator/"
+    ".tmux/plugins/tpm/"
 )
 
 # why not wokring with lua dir?
 ln --symbolic "$(pwd)/.config/lvim/lua/" --target-directory="${HOME}/.config/lvim/" &&
 
 for item in ${dirs_to_link[@]}; do
-  from="$(pwd)/$item"
-  to="$HOME/$item"
+    from="$(pwd)/$item"
+    to="$HOME/$item"
 
-  filename=$(basename "$from")
+    filename=$(basename "$from")
 
-  echo "DIRECTORY: $from -> $to"
-  ln --symbolic "$from" --target-directory="$to" &&
-  rm -rf $from/$filename
+    echo "DIRECTORY: $from -> $to"
+    ln --symbolic "$from" --target-directory="$to" &&
+    rm -rf $from/$filename
 done
 
 # ----------------------------------------------------------------------------------------------------
 
 files_to_link=(
-  ".config/alacritty/alacritty.toml"
-  ".config/lvim/config.lua"
-  ".config/lvim/spell/de.utf-8.spl"
-  ".config/lvim/spell/de.utf-8.sug"
-  ".config/vale/.vale.ini"
-  ".local/share/lunarvim/site/pack/lazy/opt/nvim-treesitter/spell/en.utf-8.add"
-  ".local/share/lunarvim/site/pack/lazy/opt/nvim-treesitter/spell/en.utf-8.add.spl"
-  ".antigenrc"
-  ".tmux.conf"
-  ".zshrc"
+    ".config/alacritty/alacritty.toml"
+    ".config/lvim/config.lua"
+    ".config/lvim/spell/de.utf-8.spl"
+    ".config/lvim/spell/de.utf-8.sug"
+    ".config/vale/.vale.ini"
+    ".local/share/lunarvim/site/pack/lazy/opt/nvim-treesitter/spell/en.utf-8.add"
+    ".local/share/lunarvim/site/pack/lazy/opt/nvim-treesitter/spell/en.utf-8.add.spl"
+    ".antigenrc"
+    ".tmux.conf"
+    ".zshrc"
 )
 
 for item in ${files_to_link[@]}; do
-  from="$(pwd)/$item"
-  to=$HOME/$item
-  echo "FILE: $from -> $to"
-  ln --symbolic --force "$from" "$to"
+    from="$(pwd)/$item"
+    to=$HOME/$item
+    echo "FILE: $from -> $to"
+    ln --symbolic --force "$from" "$to"
 done
