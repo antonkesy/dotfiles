54ea236d6adb15fa3439521d7eb6eb74aed9e0e7
Anton Kesy <antonkesy@gmail.com>
2023-11-10 10:38:24 +0100

    make easier copy

--- install_scripts/terminal.sh
+++ install_scripts/terminal.sh
@@ -4,9 +4,10 @@ sudo apt install tmux fzf xsel exa fd-find atop btop
 pip install trash-cli
 
 # unbind alt to avoaid conflict with tmux
-gsettings set org.gnome.desktop.wm.keybindings activate-window-menu "[]"
-gsettings set org.gnome.desktop.wm.keybindings switch-group "[]"
+gsettings set org.gnome.desktop.wm.keybindings activate-window-menu "[]" && \
+gsettings set org.gnome.desktop.wm.keybindings switch-group "[]" && \
 gsettings set org.gnome.desktop.wm.keybindings switch-group-backward "[]"
+
 
 # Requires built_from_source/alacritty/install.sh
 sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator $(which alacritty) 50
