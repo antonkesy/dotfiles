4c3006aa564fc473df7b5274d784b1e8b33a12a2
Anton Kesy <antonkesy@gmail.com>
2023-12-02 00:00:45 +0100

    remove neovim submodule

--- built_from_source/neovim/install.sh
+++ built_from_source/neovim/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install ninja-build gettext cmake unzip curl  cmake
+sudo apt-get install -y git ninja-build gettext cmake unzip curl cmake &&
+git clone --branch release-0.9 https://github.com/neovim/neovim.git &&
 cd neovim && 
 sudo make CMAKE_BUILD_TYPE=RelWithDebInfo &&
 sudo make install &&
