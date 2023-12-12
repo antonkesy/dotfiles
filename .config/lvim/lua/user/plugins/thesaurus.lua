0e929a4457ba369fae23ccf904cbe512dc9b1800
Anton Kesy <anton@kesy.de>
2023-12-12 15:56:13 +0100

    move lvim config into submodule

--- .config/lvim/lua/user/plugins/thesaurus.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "Ron89/thesaurus_query.vim",
-    config = function()
-      vim.api.nvim_set_keymap('n', '<Leader>ts', [[:ThesaurusQueryReplaceCurrentWord<CR>]],
-        { noremap = true, silent = true })
-    end,
-  }
-}
