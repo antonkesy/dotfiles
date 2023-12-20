12386dd7df24db8bebb81f81476e1c98ef720983
Anton Kesy <anton@kesy.de>
2023-12-20 15:35:00 +0100

    remove i3

--- install_scripts/i3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget -O- https://baltocdn.com/i3-window-manager/signing.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/i3wm-signing.gpg && \
-sudo apt install apt-transport-https --yes && \
-sudo apt update && \
-sudo apt install feh fonts-font-awesome rofi pulseaudio-utils xbacklight alsa-tools clipit gcc git terminator locate pcmanfm acpi libnotify-bin htop
-sudo add-apt-repository -y -u ppa:linuxuprising/shutter
-sudo apt install shutter
-sudo apt install i3 --yes
