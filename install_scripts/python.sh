c8b5c9bf4dca5e975bc0ada888adff292652fe51
Anton Kesy <anton@kesy.de>
2023-12-23 23:03:29 +0100

    clean up install

--- install_scripts/python.sh
+++ install_scripts/python.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sudo apt install python3.11-dev python3-pip python3.11-venv
-# TODO set python version
+sudo apt install python3.13-dev python3-pip python3.13-venv
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.13 1
+sudo update-alternatives --set python /usr/bin/python3.13
