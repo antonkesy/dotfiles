7abd1181dc3ea4c2f07ba66b1dcb79285c800e38
Anton Kesy <anton@kesy.de>
2023-12-23 23:47:06 +0100

    update ros2 script

--- install_scripts/ros2.sh
+++ install_scripts/ros2.sh
@@ -7,7 +7,10 @@ rm ./nomachine*
 
 # webots 
 # https://cyberbotics.com/doc/guide/installation-procedure#installing-the-debian-package-with-the-advanced-packaging-tool-apt
-wget -qO- https://cyberbotics.com/Cyberbotics.asc | sudo apt-key add -
-sudo apt-add-repository 'deb https://cyberbotics.com/debian/ binary-amd64/'
+sudo mkdir -p /etc/apt/keyrings
+cd /etc/apt/keyrings || exit
+sudo wget -q https://cyberbotics.com/Cyberbotics.asc
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/Cyberbotics.asc] https://cyberbotics.com/debian binary-amd64/" | sudo tee /etc/apt/sources.list.d/Cyberbotics.list
+cd - || exit
 sudo apt-get update
 sudo apt-get install webots -y
