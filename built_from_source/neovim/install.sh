be27558521b600cd5f2ea105cf175505da1db966
Anton Kesy <anton@kesy.de>
2023-12-23 22:47:09 +0100

    Revert "remove neovim submodule"
    This reverts commit 4c3006aa564fc473df7b5274d784b1e8b33a12a2.

--- built_from_source/neovim/install.sh
+++ built_from_source/neovim/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install -y git ninja-build gettext cmake unzip curl cmake &&
-git clone --branch release-0.9 https://github.com/neovim/neovim.git &&
+sudo apt-get install ninja-build gettext cmake unzip curl  cmake
 cd neovim && 
 sudo make CMAKE_BUILD_TYPE=RelWithDebInfo &&
 sudo make install &&
