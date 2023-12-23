5fa869e5b857273972f41d8b2f635948cdaef0b0
Anton Kesy <anton@kesy.de>
2023-12-23 22:33:06 +0100

    add disk format

--- install_scripts/misc.sh
+++ install_scripts/misc.sh
@@ -4,3 +4,5 @@
 sudo apt install -y ffmpeg flex pandoc pdfarranger thunderbird libreoffice gimp baobab curl qt6-base-dev anki python3-distutils latexmk texstudio texlive-full ubuntu-restricted-extras
 sudo snap install krita postman obs-studio
 pip install pre-commit
+
+sudo apt-get install exfat-fuse ntfs-3g -y # disk format support
