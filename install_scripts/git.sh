c064ac1db2c130a36342eb56fcc482ec9e4276c4
Anton Kesy <anton@kesy.de>
2023-12-24 16:10:52 +0100

    add git key

--- install_scripts/git.sh
+++ install_scripts/git.sh
@@ -8,3 +8,7 @@ git config --global help.autocorrect 10
 git config --global bash.showDirtyState true
 git config --global bash.showUntrackedFiles true
 git config --global pull.rebase true
+
+git config commit.gpgsign true
+gpg --list-secret-keys --keyid-format=long
+echo "type: 'git config --global user.signingkey KEYID'"
