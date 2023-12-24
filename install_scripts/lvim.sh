b695993db8b68edfe31f5ee466119e64624adf32
Anton Kesy <anton@kesy.de>
2023-12-24 16:09:03 +0100

    install with pip

--- install_scripts/lvim.sh
+++ install_scripts/lvim.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # https://www.lunarvim.org/docs/installation
 
+sudo apt install python3-pynvim
 bash ~/.local/share/lunarvim/lvim/utils/installer/uninstall.sh
 
 sudo apt-get -y install luarocks xclip && \
--- install_scripts/python.sh
+++ install_scripts/python.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
 sudo apt install python3-pip pipx
 #sudo apt install python3.13-dev python3-pip python3.13-venv
 #sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.13 1
