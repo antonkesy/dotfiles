91d468ee8db1557e3b2ff09b18b1bdd9f0d0318b
Anton Kesy <anton@kesy.de>
2023-12-24 23:03:03 +0100

    fix dir links

--- link.sh
+++ link.sh
@@ -1,38 +1,27 @@
 #!/bin/bash
 
+dirs_to_link=(
+    ".config/lvim/lua/"
+    ".repos/antigen/"
+    ".tmux/plugins/tpm/"
+)
+
+for item in ${dirs_to_link[@]}; do
+    from="$(pwd)/$item"
+    to="$HOME/$(dirname $item)"
+    dirname=$(basename "$from")
+
+    echo "DIRECTORY: $from -> $to"
+    mkdir -p ${to}
+    unlink ${to}/${dirname}
+    ln --symbolic "${from}" --target-directory="${to}"
+done
+
+# ----------------------------------------------------------------------------------------------------
+
 mkdir -p "$HOME/.config/alacritty"
 mkdir -p "$HOME/.config/lvim/spell"
 mkdir -p "$HOME/.local/share/lunarvim/site/pack/lazy/opt/nvim-treesitter/spell"
-
-# dirs_to_link=(
-#     ".config/lvim/lua/"
-#     ".repos/antigen/"
-#     ".tmux/plugins/tpm/"
-# )
-
-# TODO: fix this in function below
-mkdir -p "${HOME}/.config/lvim/"
-ln --symbolic "$(pwd)/.config/lvim/lua/" "${HOME}/.config/lvim/"
-
-mkdir -p "$HOME/.repos"
-ln --symbolic "$(pwd)/.repos/antigen/" "${HOME}/.repos"
-
-mkdir -p "$HOME/.tmux/plugins"
-ln --symbolic "$(pwd)/.tmux/plugins/tpm/" "${HOME}/.tmux/plugins"
-
-# for item in ${dirs_to_link[@]}; do
-#     from="$(pwd)/$item"
-#     to="$HOME/$item"
-
-#     filename=$(basename "$from")
-
-#     echo "DIRECTORY: $from -> $to"
-#     # TODO: remove dir name from "to" -> 
-#     ln --symbolic "$from" --target-directory="$to" &&
-#     rm -rf $from/$filename
-# done
-
-# ----------------------------------------------------------------------------------------------------
 
 files_to_link=(
     ".config/alacritty/alacritty.toml"
