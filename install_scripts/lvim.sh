bdc899659510f8f88be22818f8ec822f081b87dd
Anton Kesy <antonkesy@gmail.com>
2023-12-24 22:27:34 +0100

    add lvim dependencies

--- install_scripts/lvim.sh
+++ install_scripts/lvim.sh
@@ -7,3 +7,9 @@ bash ~/.local/share/lunarvim/lvim/utils/installer/uninstall.sh
 sudo apt-get -y install luarocks xclip && \
 bash <(curl -s https://raw.githubusercontent.com/ChristianChiarulli/LunarVim/master/utils/installer/install.sh) --no-interaction --yes && \
 pip install neovim
+
+# LSP Dependencies
+sudo apt install nodejs npm -y
+sudo apt-get install ruby-dev -y
+sudo apt install r-base -y
+sudo apt install php libapache2-mod-php php-cli php-cgi unzip -y
