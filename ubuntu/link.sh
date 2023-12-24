26464d6d786a1b28cf7ac94bf70de14bf5f7d519
Anton Kesy <antonkesy@gmail.com>
2023-12-24 10:21:09 +0100

    add gdm3 config

--- ubuntu/link.sh
+++ ubuntu/link.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+echo "WARNING: run this script from dotfiles repository root directory"
 sudo ln --symbolic --force "$(pwd)/ubuntu/etc/default/grub" "/etc/default/grub"
+sudo ln --symbolic --force "$(pwd)/ubuntu/etc/gdm3/custom.conf" "/etc/gdm3/custom.conf"
