32b775a02eb9dfb80c57f792543e23f99eb23983
Anton Kesy <antonkesy@gmail.com>
2023-12-25 19:45:33 +0100

    cancle failed all

--- all.sh
+++ all.sh
@@ -2,10 +2,10 @@
 
 SOURCE_ANY_SHELL=". ~/.bashrc || . ~/.zshrc"
 
-./install_scripts/base.sh
-./link.sh
+./install_scripts/base.sh &&
+./link.sh &&
 ${SOURCE_ANY_SHELL}
-./built_from_source/neovim/install.sh
-./built_from_source/alacritty/install.sh
+./built_from_source/neovim/install.sh &&
+./built_from_source/alacritty/install.sh &&
 ${SOURCE_ANY_SHELL}
 ./install_scripts/non_interactive.sh
