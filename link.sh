b1cab5f8ec3c542e97162bd434605808caaac99c
Anton Kesy <antonkesy@gmail.com>
2023-12-25 22:31:55 +0100

    add alacritty themes

--- link.sh
+++ link.sh
@@ -2,28 +2,27 @@
 
 dirs_to_link=(
     ".config/lvim/lua/"
+    ".config/alacritty/"
     ".repos/antigen/"
     ".tmux/plugins/tpm/"
 )
 
 for item in ${dirs_to_link[@]}; do
     from="$(pwd)/$item"
-    to="$HOME/$(dirname $item)"
+    to="$HOME/$(dirname "$item")"
     dirname=$(basename "$from")
 
     echo "DIRECTORY: $from -> $to"
-    mkdir -p ${to}
-    unlink ${to}/${dirname}
+    mkdir -p "${to}"
+    unlink "${to}"/"${dirname}"
     ln --symbolic "${from}" --target-directory="${to}"
 done
 
 # ----------------------------------------------------------------------------------------------------
 
-mkdir -p "$HOME/.config/alacritty"
 mkdir -p "$HOME/.config/lvim/spell"
 
 files_to_link=(
-    ".config/alacritty/alacritty.toml"
     ".config/lvim/config.lua"
     ".antigenrc"
     ".tmux.conf"
