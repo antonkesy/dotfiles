de8c17fc719c061bb74aea42ed9d835ef94bfa39
Anton Kesy <antonkesy@gmail.com>
2023-12-25 17:44:34 +0100

    dont stop on failed dependencies

--- install_scripts/non_interactive/lvim.sh
+++ install_scripts/non_interactive/lvim.sh
@@ -4,8 +4,8 @@
 apt install python3-pynvim
 bash ~/.local/share/lunarvim/lvim/utils/installer/uninstall.sh
 
-apt-get -y install luarocks xclip && \
-bash <(curl -s https://raw.githubusercontent.com/ChristianChiarulli/LunarVim/master/utils/installer/install.sh) --no-interaction --yes && \
+apt-get -y install luarocks xclip
+bash <(curl -s https://raw.githubusercontent.com/ChristianChiarulli/LunarVim/master/utils/installer/install.sh) --no-interaction --yes
 pip install neovim
 
 # LSP Dependencies
