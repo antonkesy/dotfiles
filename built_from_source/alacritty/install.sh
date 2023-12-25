46950d7a5812ab35da63021328210d1dcc605823
Anton Kesy <antonkesy@gmail.com>
2023-12-25 17:15:32 +0100

    use relative path from script

--- built_from_source/alacritty/install.sh
+++ built_from_source/alacritty/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 &&
-#rustup override set stable &&
-#rustup update stable &&
-cd alacritty && 
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+rustup override set stable &&
+rustup update stable &&
+cd ${SCRIPT_DIR}/alacritty &&
 cargo build --release &&
 chmod +x target/release/alacritty &&
 cp ./target/release/alacritty /usr/local/bin &&
