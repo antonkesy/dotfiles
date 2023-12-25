011371d1ae10cf93e68ad4adf55c9b9fa09b6452
Anton Kesy <anton@kesy.de>
2023-12-25 19:35:00 +0100

    explicit source

--- all.sh
+++ all.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+SOURCE_ANY_SHELL=". ~/.bashrc || . ~/.zshrc"
+
 ./install_scripts/base.sh
 ./link.sh
-# Source any shell env
-. ~/.bashrc
-. ~/.zshrc
+${SOURCE_ANY_SHELL}
 ./built_from_source/neovim/install.sh
 ./built_from_source/alacritty/install.sh
+${SOURCE_ANY_SHELL}
+./install_scripts/non_interactive.sh
