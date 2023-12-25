0ae14da83be280facedc09de9eeacb2d980ed4b0
Anton Kesy <antonkesy@gmail.com>
2023-12-25 16:58:46 +0100

    add broken info

--- install_scripts/base/java.sh
+++ install_scripts/base/java.sh
@@ -5,6 +5,7 @@ apt install -y maven openjdk-21-jdk
 # gradle
 curl -s "https://get.sdkman.io" | bash
 source "${HOME}/.sdkman/bin/sdkman-init.sh"
+# not working correctly
 sdk selfupdate force
 sdk install gradle
 
