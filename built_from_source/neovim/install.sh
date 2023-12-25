154be779b03d7cd9058a115b8edb65976daab4d3
Anton Kesy <anton@kesy.de>
2023-12-25 17:15:32 +0100

    use relative path from script

--- built_from_source/neovim/install.sh
+++ built_from_source/neovim/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-apt-get install ninja-build gettext cmake unzip curl  cmake
-cd neovim && 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+apt-get install -y ninja-build gettext cmake unzip curl cmake
+cd ${SCRIPT_DIR}/neovim &&
 make CMAKE_BUILD_TYPE=RelWithDebInfo &&
 make install &&
 cd ..
