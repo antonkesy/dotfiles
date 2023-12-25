a7ee65a8b1a3adfd298392877530ffa0a60f4ecf
Anton Kesy <antonkesy@gmail.com>
2023-12-25 11:34:19 +0100

    remove spelling

--- link.sh
+++ link.sh
@@ -21,15 +21,10 @@ done
 
 mkdir -p "$HOME/.config/alacritty"
 mkdir -p "$HOME/.config/lvim/spell"
-mkdir -p "$HOME/.local/share/lunarvim/site/pack/lazy/opt/nvim-treesitter/spell"
 
 files_to_link=(
     ".config/alacritty/alacritty.toml"
     ".config/lvim/config.lua"
-    ".config/lvim/spell/de.utf-8.spl"
-    ".config/lvim/spell/de.utf-8.sug"
-    ".local/share/lunarvim/site/pack/lazy/opt/nvim-treesitter/spell/en.utf-8.add"
-    ".local/share/lunarvim/site/pack/lazy/opt/nvim-treesitter/spell/en.utf-8.add.spl"
     ".antigenrc"
     ".tmux.conf"
     ".zshrc"
