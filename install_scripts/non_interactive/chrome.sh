86558ec575d4a5d08b44c600fb96365006c2dc39
Anton Kesy <antonkesy@gmail.com>
2023-12-25 19:58:37 +0100

    add dependency

--- install_scripts/non_interactive/chrome.sh
+++ install_scripts/non_interactive/chrome.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # https://itsfoss.com/install-chrome-ubuntu/
+apt-get install -y wget
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 rm ./google-chrome-stable_current_amd64.deb
