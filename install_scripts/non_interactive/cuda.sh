98092633de41fd9e745a0247aff041b0f703377c
Anton Kesy <antonkesy@gmail.com>
2023-12-25 22:14:47 +0100

    add grahpics driver PPA

--- install_scripts/non_interactive/cuda.sh
+++ install_scripts/non_interactive/cuda.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # working nvidia driver with 22.10 Ubuntu and Razer Core X Chroma
+add-apt-repository ppa:graphics-drivers/ppa -y
+apt update -y
+apt upgrade -y
+apt remove 'nvidia-*' -y
 apt-get install nvidia-driver-535-open nvidia-utils-535 nvidia-cuda-toolkit -y
 prime-select nvidia
