4ce2b9f3a4d7fa22daa7e391a69ca32fa786a95e
Anton Kesy <antonkesy@gmail.com>
2023-12-25 17:07:26 +0100

    add build-essentials

--- install_scripts/base/misc.sh
+++ install_scripts/base/misc.sh
@@ -2,6 +2,6 @@
 # all small packages without grouping
 
 apt-get install git -y
-DEBIAN_FRONTEND=noninteractive apt install -y ffmpeg pandoc pdfarranger curl qt6-base-dev python3-distutils ubuntu-restricted-extras
+DEBIAN_FRONTEND=noninteractive apt install -y ffmpeg pandoc pdfarranger curl qt6-base-dev python3-distutils ubuntu-restricted-extras build-essential
 apt-get install -y exfat-fuse ntfs-3g # disk format support
 apt-get -y ibwebkit2gtk-4.0-37 # Cisco AnyConnect
