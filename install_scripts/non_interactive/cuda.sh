16476d00e4d8fdf8c43ff5d2b598c8f11b905226
Anton Kesy <antonkesy@gmail.com>
2023-12-25 19:58:43 +0100

    merge apt install

--- install_scripts/non_interactive/cuda.sh
+++ install_scripts/non_interactive/cuda.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 # working nvidia driver with 22.10 Ubuntu and Razer Core X Chroma
-apt install nvidia-driver-535-open nvidia-utils-535 -y
-
-apt install nvidia-cuda-toolkit -y
+apt-get install nvidia-driver-535-open nvidia-utils-535 nvidia-cuda-toolkit -y
 prime-select nvidia
