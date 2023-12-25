858bb83ce11a7ec02f27211c8dddfeea65d1f704
Anton Kesy <antonkesy@gmail.com>
2023-12-25 17:01:44 +0100

    non-interactive

--- install_scripts/base/homebrew.sh
+++ install_scripts/base/homebrew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # https://brew.sh/
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "NONINTERACTIVE=1 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
