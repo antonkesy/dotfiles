10686bcf7605adf4b51b25c7062f2eff3cc1ea18
Anton Kesy <anton@kesy.de>
2024-03-18 13:25:25 +0100

    improve python install

--- install_scripts/base/python.sh
+++ install_scripts/base/python.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-rm /usr/lib/python3.11/EXTERNALLY-MANAGED
-apt install -y python3-pip pipx
+VERSION="3.11"
+
+rm /usr/lib/python${VERSION}/EXTERNALLY-MANAGED
+apt install -y python3-pip pipx python${VERSION}-full python${VERSION}-dev
 pip install pre-commit
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.${VERSION} 1
