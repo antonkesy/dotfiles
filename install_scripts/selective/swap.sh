0a288c19349bc211c55240417ee4f004da5d28e5
Anton Kesy <antonkesy@gmail.com>
2024-10-10 11:38:11 +0200

    improve swap creation

--- install_scripts/selective/swap.sh
+++ install_scripts/selective/swap.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sudo fallocate -l 100G /swapfile &&
-sudo ls -lh /swapfile &&
-sudo chmod 600 /swapfile &&
-sudo mkswap /swapfile &&
-sudo swapon /swapfile &&
-sudo cp /etc/fstab /etc/fstab.bak &&
-sudo echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+SWAP_FILE=/swapfile
+
+sudo fallocate -l 100G "$SWAP_FILE"
+sudo chmod 600 "$SWAP_FILE"
+sudo mkswap "$SWAP_FILE"
+sudo swapon "$SWAP_FILE"
+echo "$SWAP_FILE none swap sw 0 0" | sudo tee -a /etc/fstab
+sudo swapon --show
