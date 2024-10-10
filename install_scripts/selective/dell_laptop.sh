f498d6d3ff939c56beb0f2177707807b4ce1f4ad
Anton Kesy <anton@kesy.de>
2024-10-10 20:37:25 +0200

    add dell powermanager

---
+++ install_scripts/selective/dell_laptop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://github.com/alexVinarskis/dell-powermanager/releases/download/0.10.0/dell-powermanager_0.10.0+20240225-201753_amd64.deb
+sudo apt install ./dell-powermanager_0.10.0+20240225-201753_amd64.deb -y
+rm dell-powermanager_0.10.0+20240225-201753_amd64.deb
