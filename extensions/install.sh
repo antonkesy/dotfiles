418be20c4ad8f3e5139d42eb03b302306bc34619
Anton Kesy <antonkesy@gmail.com>
2024-10-10 21:05:59 +0200

    copy extension instead of link

--- extensions/install.sh
+++ extensions/install.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-sudo apt-get install gnome-browser-connector
-sudo apt install gnome-shell-extension-manager
+sudo apt-get install -y gnome-browser-connector gnome-terminal gnome-shell-extension-manager
 
 # TODO: https://extensions.gnome.org/extension/4937/draw-on-you-screen-2
 
-INSTALL_DIR=~/.local/share/gnome-shell/extensions/
+INSTALL_DIR=~/.local/share/gnome-shell/extensions
 
 # https://github.com/GnomeSnapExtensions/gSnap
-ln -s ./gSnap ${INSTALL_DIR}/gSnap@micahosborne
+cp -rf ./gSnap ${INSTALL_DIR}/gSnap@micahosborne
 gnome-extensions enable gSnap@micahosborne
 
 # https://extensions.gnome.org/extension/779/clipboard-indicator/
-ln -s ./gnome-shell-extension-lockkeys ${INSTALL_DIR}/clipboard-indicator@tudmotu.com
+cp -rf ./gnome-shell-extension-clipboard-indicator ${INSTALL_DIR}/clipboard-indicator@tudmotu.com
 gnome-extensions enable clipboard-indicator@tudmotu.com
 
 # https://extensions.gnome.org/extension/36/lock-keys/
-ln -s ./gnome-shell-extension-lockkeys ${INSTALL_DIR}/lockkeys@vaina.lt
+cp -rf ./gnome-shell-extension-lockkeys/lockkeys@vaina.lt/ ${INSTALL_DIR}/lockkeys@vaina.lt
 gnome-extensions enable lockkeys@vaina.lt
