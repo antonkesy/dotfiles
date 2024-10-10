2e9f7ef0f700cc53164b3d1c93c05a0aff5e946b
Anton Kesy <anton@kesy.de>
2024-10-10 20:09:08 +0200

    add quickemu

--- install_scripts/selective/vm.sh
+++ install_scripts/selective/vm.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 sudo apt-get install -y virtualbox
+
+sudo apt-add-repository ppa:flexiondotorg/quickemu -y
+sudo apt update -y
+sudo apt install quickemu -y
+
+# https://github.com/quickemu-project/quickemu/wiki/04-Create-Windows-virtual-machines
+mkdir ~/quickemu -p && cd ~/quickemu || exit
+quickget windows 10
+# quickemu --vm windows-10.conf
+quickget windows 11
+# quickemu --vm windows-11.conf
+cd - || exit
