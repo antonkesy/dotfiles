43bff9f9455fc7d1063cf4973dc145e9e5504ffc
Anton Kesy <anton@kesy.de>
2024-10-21 09:53:55 +0200

    add benchmarkers

--- install_scripts/base/c++.sh
+++ install_scripts/base/c++.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install -y google-mock googletest cppcheck libboost-all-dev valgrind libgtest-dev
+sudo apt-get install -y google-mock googletest cppcheck libboost-all-dev valgrind libgtest-dev linux-tools-common linux-tools-generic
+sudo snap install kcachegrind
