2f006e290d6bec002051676aaa87a5a988d13fd8
Anton Kesy <anton@kesy.de>
2024-11-23 20:59:49 +0100

    add bear

--- install_scripts/base/c++.sh
+++ install_scripts/base/c++.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install -y google-mock googletest cppcheck libboost-all-dev valgrind libgtest-dev linux-tools-common linux-tools-generic
+sudo apt-get install -y google-mock googletest cppcheck libboost-all-dev valgrind libgtest-dev linux-tools-common linux-tools-generic bear
 sudo snap install kcachegrind
