6cf4a371952cdb4d50ed3758076168b2775dcb2b
Anton Kesy <anton@kesy.de>
2024-11-23 21:52:39 +0100

    add nice to have tools

--- install_scripts/base/c++.sh
+++ install_scripts/base/c++.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install -y google-mock googletest cppcheck libboost-all-dev valgrind libgtest-dev linux-tools-common linux-tools-generic bear
+sudo apt-get install -y google-mock googletest cppcheck libboost-all-dev valgrind libgtest-dev linux-tools-common linux-tools-generic bear meson
 sudo snap install kcachegrind
