f5b37d7f7c4c80713024485b3343686c91c925e1
Anton Kesy <anton@kesy.de>
2024-11-25 16:04:38 +0100

    add oneapi mkl

--- install_scripts/base/c++.sh
+++ install_scripts/base/c++.sh
@@ -2,3 +2,13 @@
 
 sudo apt-get install -y google-mock googletest cppcheck libboost-all-dev valgrind libgtest-dev linux-tools-common linux-tools-generic bear meson
 sudo snap install kcachegrind
+
+# SYCL
+
+# https://www.intel.com/content/www/us/en/developer/tools/oneapi/onemkl-download.html?operatingsystem=linux&linux-install=apt
+sudo apt update
+sudo apt install -y gpg-agent wget
+wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+sudo apt update -y
+sudo apt install intel-oneapi-mkl-devel -y
