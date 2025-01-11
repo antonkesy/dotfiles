41b1cd38da98d38a40cac1de8a0db41d73341e20
Anton Kesy <anton@kesy.de>
2025-01-11 22:52:07 +0100

    add jupyter install

--- install_scripts/base/python.sh
+++ install_scripts/base/python.sh
@@ -6,3 +6,5 @@ sudo rm /usr/lib/python${VERSION}/EXTERNALLY-MANAGED -f
 sudo apt install -y python3-pip pipx python${VERSION}-full python${VERSION}-dev
 pip install pre-commit
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.${VERSION} 1
+
+pip install jupyterlab notebook
