980619b323629eb73be18267f6ae004e3e19e1bb
Anton Kesy <anton@kesy.de>
2025-02-24 10:01:15 +0100

    sanitzie file names

--- usefull_scripts/youtube-download-music-playlist.sh
+++ usefull_scripts/youtube-download-music-playlist.sh
@@ -7,16 +7,20 @@ if [ $# -eq 0 ]; then
 fi
 youtube_link="$1"
 
+
 # get playlist title and create directory
-playlist_title=$(yt-dlp --skip-download --print playlist_title --no-warnings -I 1:1 ${youtube_link})
+playlist_title=$(yt-dlp --skip-download --print playlist_title --no-warnings -I 1:1 "${youtube_link}")
+
+# Sanitize playlist title (replace problematic characters)
+playlist_title=$(echo "$playlist_title" | tr -d '"/:?*<>|')
 
 if [ -z "$playlist_title" ]; then
     echo "Could not fetch playlist title :/ Set directory name to timecode"
     playlist_title=$(date +%s)
 fi
 
-mkdir "$playlist_title"
+mkdir -p "$playlist_title"
 cd "$playlist_title" || exit
 
 # download playlist
-yt-dlp --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 160K --output "%(title)s.%(ext)s" --yes-playlist "${youtube_link}"
+yt-dlp --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 160K --output "%(title)s.%(ext)s" --yes-playlist --restrict-filenames "${youtube_link}"
