81308733a9bd05a9a1946c2e3d92326b56e3ae6e
Anton Kesy <anton@kesy.de>
2025-04-10 14:47:56 +0200

    split repository install

--- install_scripts/base/repositories.sh
+++ install_scripts/base/repositories.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo add-apt-repository universe multiverse restricted -y
+sudo add-apt-repository -y universe
+sudo add-apt-repository -y multiverse
+sudo add-apt-repository -y restricted
