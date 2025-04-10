f1f7071b36804452ccb54faf33a5dfd4dddcdb9c
Anton Kesy <anton@kesy.de>
2025-04-10 17:00:08 +0200

    fix fnm sourcing

--- .config/zsh/fnm.zsh
+++ .config/zsh/fnm.zsh
@@ -1,2 +1,9 @@
+# fnm
+FNM_PATH="/home/ak/.local/share/fnm"
+if [ -d "$FNM_PATH" ]; then
+  export PATH="/home/ak/.local/share/fnm:$PATH"
+  eval "`fnm env`"
+fi
+
 # https://github.com/Schniz/fnm
 eval "$(fnm env --use-on-cd --shell zsh)"
