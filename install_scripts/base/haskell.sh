33177c8f71e814d934ee40d508c582dbe5f24d7d
Anton Kesy <anton@kesy.de>
2025-04-20 21:06:50 +0200

    fix non-interactive base install

--- install_scripts/base/haskell.sh
+++ install_scripts/base/haskell.sh
@@ -2,7 +2,7 @@
 
 # https://www.haskell.org/ghcup/
 sudo apt install build-essential curl libffi-dev libffi8ubuntu1 libgmp-dev libgmp10 libncurses-dev -y
-curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 sh
 
 # https://docs.haskellstack.org/en/stable/install_and_upgrade/
 curl -sSL https://get.haskellstack.org/ | sudo sh
--- install_scripts/base/misc.sh
+++ install_scripts/base/misc.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# all small packages without grouping
 
-sudo apt-get install git -y
-DEBIAN_FRONTEND=noninteractive sudo apt install -y ffmpeg pandoc pdfarranger curl qt6-base-dev ubuntu-restricted-extras build-essential
+sudo apt-get install -y git ffmpeg pandoc pdfarranger curl qt6-base-dev ubuntu-restricted-extras build-essential
 sudo apt-get install -y exfat-fuse ntfs-3g # disk format support
-sudo apt-get -y ibwebkit2gtk-4.0-37 # Cisco AnyConnect
+# sudo apt-get -y ibwebkit2gtk-4.0-37 # Cisco AnyConnect
