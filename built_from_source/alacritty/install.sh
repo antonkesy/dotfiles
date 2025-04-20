567c6817b311c97f80c973d78f8719fd9ce63e8c
Anton Kesy <anton@kesy.de>
2025-04-21 00:05:57 +0200

    source rust for non-interactive install

--- built_from_source/alacritty/install.sh
+++ built_from_source/alacritty/install.sh
@@ -3,8 +3,10 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+source ~/.config/zsh/rust.zsh
+
 sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
-cd ${SCRIPT_DIR}/alacritty &&
+cd "${SCRIPT_DIR}"/alacritty &&
 cargo build --release &&
 chmod +x target/release/alacritty &&
 sudo cp ./target/release/alacritty /usr/local/bin &&
