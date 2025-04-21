62478975af3ffb6c68727165719d8f50c9c1f616
Anton Kesy <anton@kesy.de>
2025-04-21 23:30:40 +0200

    remove duplicate starship install

--- install_scripts/selective/zsh.sh
+++ install_scripts/selective/zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install unzip
+sudo apt install -y unzip
 
 # hack font
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip
@@ -8,14 +8,9 @@ sudo mkdir /usr/share/fonts/truetype/hack/ -p
 sudo unzip ./Hack.zip -d /usr/share/fonts/truetype/hack/
 fc-cache -f -v
 
-# starship theme
-# https://starship.rs/
-curl -sS https://starship.rs/install.sh | sh -- --yes
-
 sudo apt install -y zsh
 # set zsh as default shell
 sudo hsh -s $(which zsh)
-
 
 # https://github.com/ohmyzsh/ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
