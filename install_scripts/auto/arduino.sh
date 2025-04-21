36585edaa2f81dd3eeaaa14a917a76dd3b2860c6
Anton Kesy <anton@kesy.de>
2025-04-22 00:09:50 +0200

    replace sh script with Makefile + split auto/manual install

--- install_scripts/auto/arduino.sh
+++ install_scripts/auto/arduino.sh
@@ -4,5 +4,6 @@
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 
 # https://arduino.github.io/arduino-cli/0.33/installation/#install-via-homebrew-macoslinux
-sudo brew update
-sudo brew install arduino-cli
+export PATH=/home/linuxbrew/.linuxbrew/bin/:$PATH
+brew update
+brew install arduino-cli
--- install_scripts/auto/docker.sh
+++ install_scripts/auto/docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # https://docs.docker.com/engine/install/ubuntu/
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get -y update
+sudo apt-get -y install ca-certificates curl gnupg
 
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -15,9 +15,9 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # post install
 sudo groupadd docker
-sudo usermod -aG docker $USER
+sudo usermod -aG docker "$USER"
 newgrp docker
