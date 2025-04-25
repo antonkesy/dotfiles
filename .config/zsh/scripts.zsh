fd375b0acb415074e7e8318ac27609ef7c7e38c9
Anton Kesy <anton@kesy.de>
2025-04-25 13:41:01 +0200

    fix multiple sudo for update-all

--- .config/zsh/scripts.zsh
+++ .config/zsh/scripts.zsh
@@ -1,2 +1,2 @@
-alias update-all='~/.config/zsh/scripts/update-all.zsh'
+alias update-all='sudo ~/.config/zsh/scripts/sudo-update-all.zsh && ~/.config/zsh/scripts/update-all.zsh'
 alias yt-list-download='~/.config/zsh/scripts/youtube-download-music-playlist.sh'
