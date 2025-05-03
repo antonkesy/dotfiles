507620e609043c399bfd191d180eaec9e00b9144
Anton Kesy <anton@kesy.de>
2025-05-03 12:16:28 +0200

    remove unity

--- install_scripts/auto/unity.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install libssl1.1
-
-#unityhub
-sh -c 'echo "deb https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
-wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
-sudo apt update -y
-sudo apt install unityhub blender -y
