7ffb24f0ffa7f125af6447681cd5e74a5fadd3a3
Anton Kesy <anton@kesy.de>
2025-05-03 12:16:37 +0200

    remove windows VM install

--- install_scripts/auto/vm.sh
+++ install_scripts/auto/vm.sh
@@ -7,9 +7,6 @@ sudo apt update -y
 sudo apt install quickemu -y
 
 # https://github.com/quickemu-project/quickemu/wiki/04-Create-Windows-virtual-machines
-mkdir ~/quickemu -p && cd ~/quickemu || exit
-quickget windows 10
+mkdir ~/quickemu -p && cd ~/quickemu && quickget windows 10 && quickget windows 11 && cd - || exit
 # quickemu --vm windows-10.conf
-quickget windows 11
 # quickemu --vm windows-11.conf
-cd - || exit
