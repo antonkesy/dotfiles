d69131f51cc47228f8126a5832a99d2b2a693a45
Anton Kesy <anton@kesy.de>
2025-05-17 19:59:44 +0200

    add cuda support for docker containers

--- install_scripts/auto/cuda.sh
+++ install_scripts/auto/cuda.sh
@@ -15,4 +15,15 @@ else
   sudo apt-get --fix-broken install nvidia-driver-${VERSION}-open nvidia-utils-${VERSION} nvidia-cuda-toolkit -y
   # only required for multiple GPUs
   sudo prime-select nvidia || echo "Error Okay; Only required for multiple GPUs; Not Docker"
+
+  # Docker support
+	curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey \
+		| gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    tee /etc/apt/sources.list.d/nvidia-container-toolkit.list \
+  && \
+    sudo apt-get update && \
+	sudo apt-get install -y nvidia-container-toolkit
+	sudo nvidia-ctk runtime configure --runtime=docker
 fi
