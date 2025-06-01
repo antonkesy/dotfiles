18af82d4f7c2a02241ba74a57050dc6ce1d49034
Anton Kesy <anton@kesy.de>
2025-06-01 13:36:46 +0200

    fix alias
    finally trash-cli with autocomplete

--- home/.config/zsh/alias.zsh
+++ home/.config/zsh/alias.zsh
@@ -1,23 +1,25 @@
-# ALIAS
+alias please="sudo"
+alias pls="sudo"
+
+# shortcuts
 alias m="make"
-# alias htop=btop
 alias g="lazygit"
-# alias cat="bat"
+alias n="nvim"
+
+# replace common tools
 alias l="exa -lah"
 alias la="exa -lah"
 alias exa="exa -lah"
-# alias du="dust"
-# alias find="fdfind"
-# alias ps="procs -t"
+alias rm="trash"
+alias mr="trash-restore"
+
+# lvim
 alias lvim="${HOME}/.local/bin/lvim"
 alias vim="lvim"
 alias neovim="lvim"
 alias nvim="lvim"
-alias n="nvim"
-# alias n.="nvim ."
+
+# python
 alias python=python3
 alias py=python
 alias py3=python3
-
-# use trash-cli instead of rm
-# alias rm="trash-put" # rather lose files than no auto-complete TODO: fix auto-complete
