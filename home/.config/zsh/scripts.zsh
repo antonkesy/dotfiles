465c12d2aa3896fab65a8590fc90a28889e345e8
Anton Kesy <anton@kesy.de>
2025-07-21 16:56:34 +0200

    download video playlist

--- home/.config/zsh/scripts.zsh
+++ home/.config/zsh/scripts.zsh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 
 alias update-all='sudo ~/.config/zsh/scripts/sudo-update-all.zsh && ~/.config/zsh/scripts/update-all.zsh'
-alias yt-list-download='~/.config/zsh/scripts/youtube-download-music-playlist.sh'
+alias yt-music-list-download='~/.config/zsh/scripts/youtube-download-music-playlist.sh'
+alias yt-video-list-download='~/.config/zsh/scripts/youtube-download-video-playlist.sh'
