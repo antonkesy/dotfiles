788f8d9ea17b3a3aa46c7597f848d02566c36194
Anton Kesy <anton@kesy.de>
2025-12-04 00:55:54 +0100

    add android studio install

---
+++ home/.config/zsh/android.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+export ANDROID_HOME="~/Android/Sdk"
+export PATH="$PATH:$ANDROID_HOME/platform-tools"
+export PATH="$PATH:$ANDROID_HOME/emulator"
+export PATH="$PATH:/opt/android-studio/bin"
