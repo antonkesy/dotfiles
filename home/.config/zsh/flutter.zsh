937eebb506cb3e670559014bb2e3faf1acddf97d
Anton Kesy <anton@kesy.de>
2025-12-05 16:48:57 +0100

    install flutter in dotfiles directory
    assume for dotfiles to be inside ~/workspace/dotfiles

--- home/.config/zsh/flutter.zsh
+++ home/.config/zsh/flutter.zsh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 
-export PATH="$PATH:/opt/flutter/bin"
+# this assumes, dotfiles are under $HOME/workspace
+export PATH="$PATH:$HOME/workspace/build/flutter/bin"
+export CHROME_EXECUTABLE=/usr/bin/chromium
