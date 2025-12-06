6fd44ec3a718a1cced650703136d6d1c13b71dc4
Anton Kesy <anton@kesy.de>
2025-12-07 00:22:19 +0100

    fix flutter path

--- home/.config/zsh/flutter.zsh
+++ home/.config/zsh/flutter.zsh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
 # this assumes, dotfiles are under $HOME/workspace
-export PATH="$PATH:$HOME/workspace/build/flutter/bin"
+export PATH="$PATH:$HOME/workspace/dotfiles/build/flutter/bin"
 export CHROME_EXECUTABLE=/usr/bin/chromium
