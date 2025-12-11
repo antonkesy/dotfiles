6a85e1c0e859c3b6de11b90f21d977e9b78f59e1
Anton Kesy <anton@kesy.de>
2025-12-11 17:27:19 +0100

    add fingerprint libs

--- postsetup/fingerprint.sh
+++ postsetup/fingerprint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo pacman -S --noconfirm --needed fprintd
+sudo pacman -S --noconfirm --needed fprintd libfprint
 sudo systemctl enable fprintd.service
 sudo systemctl start fprintd.service
+sudo pam-auth-update --enable fprintd # enable fingerprint authentication
+fprintd-enroll # enroll fingerprint for the current user
