b02dd5829dcc3d003c12d2dbe0eb54e5591697fc
Anton Kesy <anton@kesy.de>
2026-01-13 08:26:39 +0100

    shfmt

--- home/.config/zsh/scripts/youtube-download-video-playlist.sh
+++ home/.config/zsh/scripts/youtube-download-video-playlist.sh
@@ -2,11 +2,10 @@
 
 # check for arguments
 if [ $# -eq 0 ]; then
-    echo "Download YouTube Video Playlist: $0 <youtube-video-link>"
-    exit 1
+	echo "Download YouTube Video Playlist: $0 <youtube-video-link>"
+	exit 1
 fi
 youtube_link="$1"
-
 
 # get playlist title and create directory
 playlist_title=$(yt-dlp --skip-download --print playlist_title --no-warnings -I 1:1 "${youtube_link}")
@@ -15,8 +14,8 @@ playlist_title=$(yt-dlp --skip-download --print playlist_title --no-warnings -I 
 playlist_title=$(echo "$playlist_title" | tr -d '"/:?*<>|')
 
 if [ -z "$playlist_title" ]; then
-    echo "Could not fetch playlist title :/ Set directory name to timecode"
-    playlist_title=$(date +%s)
+	echo "Could not fetch playlist title :/ Set directory name to timecode"
+	playlist_title=$(date +%s)
 fi
 
 mkdir -p "$playlist_title"
@@ -24,11 +23,11 @@ cd "$playlist_title" || exit
 
 # download playlist
 yt-dlp \
-    --ignore-errors \
-    --format "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" \
-    --output "%(title)s.%(ext)s" \
-    --yes-playlist \
-    --restrict-filenames \
-    "${youtube_link}"
+	--ignore-errors \
+	--format "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" \
+	--output "%(title)s.%(ext)s" \
+	--yes-playlist \
+	--restrict-filenames \
+	"${youtube_link}"
 
 cd - || exit
--- postsetup/fingerprint.sh
+++ postsetup/fingerprint.sh
@@ -3,4 +3,4 @@ sudo pacman -S --noconfirm --needed fprintd libfprint
 sudo systemctl enable fprintd.service
 sudo systemctl start fprintd.service
 sudo pam-auth-update --enable fprintd # enable fingerprint authentication
-fprintd-enroll # enroll fingerprint for the current user
+fprintd-enroll                        # enroll fingerprint for the current user
