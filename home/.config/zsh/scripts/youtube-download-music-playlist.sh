67cce25e232e90916a2ddecaaf598671c5a9c3fd
Anton Kesy <anton@kesy.de>
2026-01-13 08:26:39 +0100

    shfmt

--- home/.config/zsh/scripts/youtube-download-music-playlist.sh
+++ home/.config/zsh/scripts/youtube-download-music-playlist.sh
@@ -2,11 +2,10 @@
 
 # check for arguments
 if [ $# -eq 0 ]; then
-    echo "Download YouTube Music Playlist: $0 <youtube-video-link>"
-    exit 1
+	echo "Download YouTube Music Playlist: $0 <youtube-video-link>"
+	exit 1
 fi
 youtube_link="$1"
-
 
 # get playlist title and create directory
 playlist_title=$(yt-dlp --skip-download --print playlist_title --no-warnings -I 1:1 "${youtube_link}")
@@ -15,8 +14,8 @@ playlist_title=$(yt-dlp --skip-download --print playlist_title --no-warnings -I 
 playlist_title=$(echo "$playlist_title" | tr -d '"/:?*<>|')
 
 if [ -z "$playlist_title" ]; then
-    echo "Could not fetch playlist title :/ Set directory name to timecode"
-    playlist_title=$(date +%s)
+	echo "Could not fetch playlist title :/ Set directory name to timecode"
+	playlist_title=$(date +%s)
 fi
 
 mkdir -p "$playlist_title"
@@ -24,14 +23,14 @@ cd "$playlist_title" || exit
 
 # download playlist
 yt-dlp \
-  --ignore-errors \
-  --format bestaudio \
-  --extract-audio \
-  --audio-format mp3 \
-  --audio-quality 160K \
-  --output "%(title)s.%(ext)s" \
-  --yes-playlist \
-  --restrict-filenames \
-  "${youtube_link}"
+	--ignore-errors \
+	--format bestaudio \
+	--extract-audio \
+	--audio-format mp3 \
+	--audio-quality 160K \
+	--output "%(title)s.%(ext)s" \
+	--yes-playlist \
+	--restrict-filenames \
+	"${youtube_link}"
 
 cd - || exit
