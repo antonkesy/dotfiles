e8df989b8402a2e8ab17986cd2b4e43ed25c3f9c
Anton Kesy <anton@kesy.de>
2026-01-14 22:29:56 +0100

    add replace script

--- home/.config/zsh/scripts.zsh
+++ home/.config/zsh/scripts.zsh
@@ -3,3 +3,4 @@
 alias update-all='sudo ~/.config/zsh/scripts/sudo-update-all.zsh && ~/.config/zsh/scripts/update-all.zsh'
 alias yt-music-list-download='~/.config/zsh/scripts/youtube-download-music-playlist.sh'
 alias yt-video-list-download='~/.config/zsh/scripts/youtube-download-video-playlist.sh'
+alias replace='~/.config/zsh/scripts/replace.zsh'
