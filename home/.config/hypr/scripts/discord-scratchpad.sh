c00a18e513af89f438fd06fd7c64c9bcd3474104
Anton Kesy <anton@kesy.de>
2026-01-15 21:07:05 +0100

    fix scratchpad

--- home/.config/hypr/scripts/discord-scratchpad.sh
+++ home/.config/hypr/scripts/discord-scratchpad.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-if ! pgrep -x discord >/dev/null; then
+if ! pgrep -x Discord >/dev/null; then
 	discord &
-	sleep 0.5
+	sleep 0.1
+else
+	# Move existing discord window to special workspace
+	hyprctl dispatch movetoworkspacesilent special:discord,class:^discord$
 fi
 
 hyprctl dispatch togglespecialworkspace discord
