f3d531927de2e78476c2f08a45fa9792507664ab
Anton Kesy <anton@kesy.de>
2026-01-29 22:36:55 +0100

    clean-up plugins

--- postsetup/hyprpm.sh
+++ postsetup/hyprpm.sh
@@ -6,10 +6,5 @@ hyprpm update
 # Add hypr-dynamic-cursors plugin
 yes | hyprpm add https://github.com/virtcode/hypr-dynamic-cursors
 hyprpm enable dynamic-cursors
-# Offical plugins
-yes | hyprpm add https://github.com/hyprwm/hyprland-plugins
-# HyprExpo2
-yes | hyprpm add https://github.com/antonkesy/hyprexpo2
-hyprpm enable hyprexpo2
 
 hyprpm reload
