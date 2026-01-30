28fdb9e936a372341bdc85dab45d867613560c76
Anton Kesy <anton@kesy.de>
2026-01-30 19:25:14 +0100

    add ollama models download

---
+++ postsetup/ollama.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ollama pull llama3.1:8b
+ollama pull deepseek-r1:8b
+ollama pull gemma3:4b
+ollama pull mistral:7b
+ollama pull qwen2.5:7b
+ollama pull phi3:3.8b
+ollama pull codellama:7b
+ollama pull deepseek-coder:6.7b
