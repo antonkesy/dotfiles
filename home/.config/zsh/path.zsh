99e0408f944dc5833fb93ea3200e1e353a46000f
Anton Kesy <anton.kesy@tu-clausthal.de>
2026-07-28 11:06:05 +0200

    workaround fix for ssh_auth_sock

--- home/.config/zsh/path.zsh
+++ home/.config/zsh/path.zsh
@@ -7,3 +7,5 @@ export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/local/bin:$PATH"
 export PATH="$HOME/.nix-profile/bin:$PATH"
+
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
