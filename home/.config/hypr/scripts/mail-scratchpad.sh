b42cc85167e61da8b74df2245cca9b6ac1117fe7
Anton Kesy <anton.kesy@tu-clausthal.de>
2026-08-02 15:09:27 +0200

    fix scratchpad after update

--- home/.config/hypr/scripts/mail-scratchpad.sh
+++ home/.config/hypr/scripts/mail-scratchpad.sh
@@ -3,9 +3,6 @@
 if ! pgrep -x thunderbird >/dev/null; then
 	thunderbird &
 	sleep 0.5
-else
-	# Move existing Thunderbird window to special workspace
-	hyprctl dispatch movetoworkspacesilent special:mail,class:org.mozilla.Thunderbird
 fi
 
-hyprctl dispatch togglespecialworkspace mail
+hyprctl dispatch 'hl.dsp.workspace.toggle_special("mail")'
