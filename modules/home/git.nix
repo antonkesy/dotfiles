c1f9e89433d626b482bf0845abdf748e2639c7a4
Anton Kesy <anton@kesy.de>
2026-09-04 09:30:06 +0200

    add signingkey

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -8,12 +8,13 @@
       user = {
         name = "Anton Kesy";
         email = "anton@kesy.de";
+        signingkey = "756091A5C7C181AE4DF5EF42EEFE6D18C482C483";
       };
       core.editor = "nvim";
       init.defaultBranch = "main";
       help.autocorrect = 10;
       pull.rebase = true;
-      # commit.gpgsign = true;
+      commit.gpgsign = true;
       bash = {
         showDirtyState = true;
         showUntrackedFiles = true;
