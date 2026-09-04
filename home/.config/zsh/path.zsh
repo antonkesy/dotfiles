7a1e0815782da9f0f6bae3c714b63237acda800b
Anton Kesy <anton@kesy.de>
2026-09-04 09:15:02 +0200

    fix ssh-path

--- home/.config/zsh/path.zsh
+++ home/.config/zsh/path.zsh
@@ -8,4 +8,4 @@ export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/local/bin:$PATH"
 export PATH="$HOME/.nix-profile/bin:$PATH"
 
-export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent"
