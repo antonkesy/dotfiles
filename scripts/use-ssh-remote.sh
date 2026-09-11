4e07c2bcbfa5a24329172361466b4458dcc9d02c
Anton Kesy <anton@kesy.de>
2026-09-11 18:33:26 +0200

    formatting

--- scripts/use-ssh-remote.sh
+++ scripts/use-ssh-remote.sh
@@ -7,16 +7,16 @@ set -eu
 url=$(git remote get-url origin 2>/dev/null) || exit 0
 
 case "$url" in
-	https://github.com/antonkesy/*)
-		repo=$(echo "$url" | sed -E 's#https://github.com/antonkesy/##; s#\.git$##')
-		new="git@github.com:antonkesy/$repo.git"
-		git remote set-url origin "$new"
-		echo "$(git rev-parse --show-toplevel): origin -> $new"
-		;;
-	git@github.com:antonkesy/*)
-		echo "$(git rev-parse --show-toplevel): origin already uses SSH: $url"
-		;;
-	*)
-		echo "$(git rev-parse --show-toplevel): origin is not an antonkesy/* GitHub URL: $url"
-		;;
+https://github.com/antonkesy/*)
+	repo=$(echo "$url" | sed -E 's#https://github.com/antonkesy/##; s#\.git$##')
+	new="git@github.com:antonkesy/$repo.git"
+	git remote set-url origin "$new"
+	echo "$(git rev-parse --show-toplevel): origin -> $new"
+	;;
+git@github.com:antonkesy/*)
+	echo "$(git rev-parse --show-toplevel): origin already uses SSH: $url"
+	;;
+*)
+	echo "$(git rev-parse --show-toplevel): origin is not an antonkesy/* GitHub URL: $url"
+	;;
 esac
